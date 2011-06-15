f785c765af1b1fab8cf4ddea52e31f2e8253bb0e
Joe Dallago <jd.dallago@gmail.com>
2011-06-15 01:01:50 -0500

    Fixed the fix_raise fixer in order to now change all instances of raise E, V and raise E() to the correct things.

--- twelve/fixes/fix_raise.py
+++ twelve/fixes/fix_raise.py
@@ -21,31 +21,42 @@ class FixRaise(fixer_base.BaseFix):
     """
 
     def transform(self, node, results):
+        syms = self.syms
+        
         exc = results['exc'].clone() if 'exc' in results else None
         val = results['val'].clone() if 'val' in results else None
         tb = results['tb'].clone() if 'tb' in results else None
 
-        exc.prefix = ''
-        
+        exc.prefix = ' '
+
         #Change Error() to Error, if there are no arguments to pass in.
         try:
             if len(exc.children[1].children) == 2:
                 exc = String(exc.children[0])
         except IndexError:
             pass
-            
-        args = [exc]
-        if val is not None:
+        
+        if tb and val:
+            exc.prefix = ''
+            args = [exc]
+
             args.append(String(", "))
             args.append(val)
+                
+            args.append(String(", "))
+            args.append(tb)
             
-            if tb is not None:
-                args.append(String(", "))
-                args.append(tb)
-        
-        raise_stmt = Call(Name("six.reraise"), args)
+            raise_stmt = Call(Name("six.reraise"), args)
+            raise_stmt.prefix = node.prefix
+            return raise_stmt
+        elif val:
+            val.prefix = ''
+            val = [val]
+            raise_stmt = pytree.Node(syms.raise_stmt,
+                                     [Name("raise"), Call(exc, val)])
+        else:
+            raise_stmt = pytree.Node(syms.raise_stmt, 
+                                     [Name("raise"), String(exc)])
+            
         raise_stmt.prefix = node.prefix
         return raise_stmt
-
-
-            
